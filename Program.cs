c674beaf491c446bd1cc42f5ef194b921f743892
Kim Hogle <kimberly.senior89@yahoo.com>
2024-08-20 12:03:22 -0400

    Commit steps 2 and 3

--- Program.cs
+++ Program.cs
@@ -7,7 +7,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        var productLogic = new ProductLogic();
+
         Console.WriteLine("Press 1 to add a product");
+        Console.WriteLine("Press 2 to view a product by name");
         Console.WriteLine("Type 'exit' to quit");
 
         string userInput = Console.ReadLine();
@@ -35,7 +38,8 @@ class Program
                     Console.Write("Is it Kitten Food? (true/false): ");
                     catFood.KittenFood = bool.Parse(Console.ReadLine());
 
-                    Console.WriteLine(JsonSerializer.Serialize(catFood));
+                    productLogic.AddProduct(catFood);
+                    Console.WriteLine("CatFood product added!");
                 }
                 else if (productType.ToLower() == "dogleash")
                 {
@@ -53,15 +57,32 @@ class Program
                     Console.Write("Enter Material: ");
                     dogLeash.Material = Console.ReadLine();
 
-                    Console.WriteLine(JsonSerializer.Serialize(dogLeash));
+                    productLogic.AddProduct(dogLeash);
+                    Console.WriteLine("DogLeash product added!");
                 }
                 else
                 {
                     Console.WriteLine("Invalid product type.");
                 }
             }
+            else if (userInput == "2")
+            {
+                Console.WriteLine("Enter the name of the product to view:");
+                string productName = Console.ReadLine();
+
+                var dogLeash = productLogic.GetDogLeashByName(productName);
+                if (dogLeash != null)
+                {
+                    Console.WriteLine(JsonSerializer.Serialize(dogLeash));
+                }
+                else
+                {
+                    Console.WriteLine("DogLeash not found.");
+                }
+            }
 
             Console.WriteLine("Press 1 to add a product");
+            Console.WriteLine("Press 2 to view a product by name");
             Console.WriteLine("Type 'exit' to quit");
             userInput = Console.ReadLine();
         }
@@ -69,7 +90,6 @@ class Program
         Console.WriteLine("Exiting application...");
     }
 }
-
 
 public class Product
 {
@@ -87,10 +107,95 @@ public class CatFood : Product
 }
 
 
+public class DryCatFood : CatFood
+{
+    public double WeightPounds { get; set; } // Moving this property to DryCatFood
+
+    public DryCatFood()
+    {
+        // Default constructor
+    }
+}
+
+
 public class DogLeash : Product
 {
     public int LengthInches { get; set; }
     public string Material { get; set; }
 }
 
+
+public class ProductLogic
+{
+    private List<Product> _products;
+    private Dictionary<string, DogLeash> _dogLeashes;
+    private Dictionary<string, CatFood> _catFoods;
+
+    public ProductLogic()
+    {
+        _products = new List<Product>();
+        _dogLeashes = new Dictionary<string, DogLeash>();
+        _catFoods = new Dictionary<string, CatFood>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+
+        if (product is DogLeash dogLeash)
+        {
+            _dogLeashes[dogLeash.Name] = dogLeash;
+        }
+        else if (product is CatFood catFood)
+        {
+            _catFoods[catFood.Name] = catFood;
+        }
+    }
+
+    public List<Product> GetAllProducts()
+    {
+        return _products;
+    }
+
+    public DogLeash GetDogLeashByName(string name)
+    {
+        try
+        {
+            return _dogLeashes[name];
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+
+    public CatFood GetCatFoodByName(string name)
+    {
+        try
+        {
+            return _catFoods[name];
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+}
+
+
+public List<Product> GetAllProducts()
+    {
+        return _products;
+    }
+
+    public DogLeash GetDogLeashByName(string name)
+    {
+        return _dogLeashes.ContainsKey(name) ? _dogLeashes[name] : null;
+    }
+
+    public CatFood GetCatFoodByName(string name)
+    {
+        return _catFoods.ContainsKey(name) ? _catFoods[name] : null;
+    }
+}
 
--- obj/Debug/net8.0/Pet Store Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pet Store Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pet Store Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36c5ec72235b919cf9d622c1f477e6e2e91c3f54")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pet Store Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pet Store Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
